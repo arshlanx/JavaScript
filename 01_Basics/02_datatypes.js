702433c433f851b22c874810ea9df90f1d614b7c
Arshlan Arfin <143599724+arshlanx@users.noreply.github.com>
2024-02-29 20:24:51 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict" ; // treat all JS code as newer version
+
+// alert(3+3) not gives output because we are using nodejs not browser
+console.log(3+3);
+
+/* DATATYPES
+    number => 2 to power 53
+    bigint
+    string => ""
+    boolean => true/false
+    null => standalone value
+    undefined => value not assigned
+    symbol => unique
+
+    object
+*/
+
+console.log(typeof "ARSHLAN")  // string
+console.log(typeof null)       // object
+console.log(typeof undefined)  // undefined
